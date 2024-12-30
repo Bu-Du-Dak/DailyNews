62137eced67c2d62701660a10a12b690256abaa4
Bu-Du-Dak <guri930219@gmail.com>
2024-12-30 16:35:49 +0900

    Auto-update 2024-12-30 news data

--- src/git_push.py
+++ src/git_push.py
@@ -1,15 +1,17 @@
 import os
 import subprocess
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 GIT_TOKEN = os.getenv('GIT_TOKEN')
 
 def git_push():
+    today = datetime.now().strftime("%Y-%m-%d")
     try:
         REPO_URL = f"https://{GIT_TOKEN}@github.com/Bu-Du-Dak/DailyNews.git"
         subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(["git", "commit", "-m", "Auto-update news data"], check=True)
+        subprocess.run(["git", "commit", "-m", f"Auto-update {today} news data"], check=True)
         subprocess.run(["git", "push", REPO_URL, "main"], check=True)
         print("GitHub에 데이터를 성공적으로 푸시했습니다!")
     except subprocess.CalledProcessError as e:
