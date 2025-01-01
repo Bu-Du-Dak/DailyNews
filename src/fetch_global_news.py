b91e76ef6386821da0609505a2aaf78322aeef6e
Hong JaeHoon <77924873+Bu-Du-Dak@users.noreply.github.com>
2025-01-01 21:22:10 +0900

    Update fetch_global_news.py

--- src/fetch_global_news.py
+++ src/fetch_global_news.py
@@ -1,6 +1,5 @@
 import os
 import requests
-from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,21 +7,18 @@ load_dotenv()
 NY_API_URL = os.getenv('NY_API_URL')
 NY_API_KEY = os.getenv('NY_API_KEY')
 
-def fetch_global_news():
-    today = datetime.utcnow().strftime('%Y%m%d')  # 오늘의 날짜 (UTC 기준)
-    params = {
-        "api-key": NY_API_KEY,
-        "begin_date": today,
-        "end_date": today
-    }
+params = {
+    "api-key": NY_API_KEY
+}
+def fetch_global_news ():
     try:
-        response = requests.get(NY_API_URL, params=params)
+        response = requests.get(NY_API_URL,params=params)
         response.raise_for_status()
-        return response.json()  
-    except requests.exceptions.RequestException as e:
+        return response.json()
+    except requests.exceptions.RequestException as e :
         print(f"Fail: {e}")
-        return None
-
+        return None  
+    
 if __name__ == "__main__":
     news_data = fetch_global_news()
     if news_data:
